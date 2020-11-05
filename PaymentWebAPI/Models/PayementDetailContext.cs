34fefd5f422d31ee59f48bc2609d5d39d154c1a3
btyas <terdayetyassine@gmail.com>
2020-11-05 16:42:14 +0100

    Ajoutez des fichiers projet.

---
+++ PaymentWebAPI/Models/PayementDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentWebAPI.Models
+{
+    public class PayementDetailContext : DbContext
+    {
+
+        public PayementDetailContext(DbContextOptions<PayementDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail>  PaymentDetails { get; set; }
+    }
+}
